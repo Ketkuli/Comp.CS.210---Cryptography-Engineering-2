be49c14e1b77778fb9c4c53aacb666780f49c782
Paavo Peltopihko <paavo.peltopihko@tuni.fi>
2024-02-24 10:39:07 +0200

    Create hp4.py
    Creation and first function sketching for further coding

---
+++ hp4.py
@@ -0,0 +1,38 @@
+"""
+Encrypt with NTRU
+
+Coursework for COMP.SEC.210
+
+Author: Paavo Peltopihko
+"""
+
+import random
+import os
+
+# This is the place to put Ex4 functions if I ever had the time to write those.
+
+def readPrivateKey():
+    # Function to read private key from a file. 
+    # Checks if there is correct file
+    # Returns the private key
+    return None
+
+def createPublicKey():
+    return None
+
+
+def encrypt():
+    return None
+
+
+
+def main():
+    # Read private key from file:
+    privateKey = readPrivateKey()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
